f84fb2d0e63f70fad06e7fbd72d9fc5fd7d0e3a6
4coma <44037654+4coma@users.noreply.github.com>
2021-04-08 16:19:30 +0200

    5. Middleware
    ! une app express est fondamentalement une succession de fonctions appelées middlewares ; chaque middleware reçoit des objets req et res peut les lire les analyser et les manipuler. les middlewares reçoivent également une méthode très importante : next, qui permet de passer au middleware suivant
    
    Le reste du code modifié est une amélioration copié collée du server.js fournie par le tuto mais non mentionnée dans la vidéo
    
    On crée quatre petits middlewares très simples dans cette app
    
    pas oublier d'utiliser next pour passer au suivant

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,27 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => 
+{
+    console.log(`requête reçue`);
+    next();
+});
+
+app.use((req, res, next) => 
+{
+    res.status(201);
+    next();
+})
+
+app.use((req, res, next) => 
+{
+    res.json({message : 'votre req a bien été reçue'});
+    next();
+});
+
 app.use((req, res) => 
 {
-    res.json({message : 'votre req a bien été reçue'})
-});
+    console.log(`la réponse a été envoyée avec succès`)
+})
 
 module.exports = app;
